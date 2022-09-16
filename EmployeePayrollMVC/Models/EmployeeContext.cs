3316af1a9a179f182392d4b509239eb6ff8eacdc
Balla <ballaphanindra@outlook.com>
2022-09-16 18:05:31 +0530

    Committing

---
+++ EmployeePayrollMVC/Models/EmployeeContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace EmployeePayRoll.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+
+        }
+        public DbSet<EmployeeModel> EmployeeDetails { get; set; }
+    }
+}
